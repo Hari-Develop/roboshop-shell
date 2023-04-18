09b1ed38ea27bbfb87e455214fe7d1e660aba9a4
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-04-18 18:14:22 +0530

    pushing the dispatch file

---
+++ dispatch.sh
@@ -0,0 +1,14 @@
+yum install golang -y
+useradd roboshop
+rm -rf /app
+mkdir /app 
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip 
+cd /app 
+unzip /tmp/dispatch.zip
+go mod init dispatch
+go get 
+go build
+cp /root/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
+systemctl daemon-reload
+systemctl enable dispatch 
+systemctl start dispatch
