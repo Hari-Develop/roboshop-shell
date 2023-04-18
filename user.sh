2a6c69c2c113710ea2c1b77e4ea02dcbead2d76d
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-04-18 16:07:00 +0530

    messages

---
+++ user.sh
@@ -0,0 +1,41 @@
+echo -e "\e[32m.....installing the repo for the node.....\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[32m.....installing the nodejs.....\e[0m"
+yum install nodejs -y
+
+echo -e "\e[32m.....add application user.....\e[0m"
+useradd roboshop
+
+echo -e "\e[32m.....making the directory.....\e[0m"
+rm -rf /app
+mkdir /app 
+
+echo -e "\e[32m.....changing the directory.....\e[0m"
+cd /app
+
+echo -e "\e[32m.....unzip the content in the app directory.....\e[0m"
+unzip /tmp/user.zip
+
+echo -e "\e[32m.....installing the npm install.....\e[0m"
+npm install
+
+echo -e "\e[32m.....adding the service file.....\e[0m"
+cp /root/roboshop-shell/user.service /etc/systemd/system/user.service
+
+echo -e "\e[32m.....starting the service file.....\e[0m"
+systemctl daemon-reload
+systemctl enable user 
+systemctl start user
+
+echo -e "\e[32m.....adding the mongo.repo.....\e[0m"
+cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[32m.....adding the mongo-shell-client.....\e[0m"
+yum install mongodb-org-shell -y
+
+echo -e "\e[32m.....adding loading scheme.....\e[0m"
+mongo --host mongodb-dev.unlockers.online </app/schema/user.js
+
+
+
