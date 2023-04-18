e7464de41b81d6e78da99f31a83d8a6dee700aa8
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-04-18 09:54:09 +0530

    code changed

--- catalogue.sh
+++ catalogue.sh
@@ -1,41 +1,17 @@
-echo -e "\e[32m.......installing nodejs package.......\e[0m"
+
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-echo -e "\e[32m.......installing nodejs.......\e[0m"
 yum install nodejs -y
-
-echo -e "\e[32m.......adding roboshop user.......\e[0m"
 useradd roboshop
-
-echo -e "\e[32m.......creating application directory.......\e[0m"
 mkdir /app 
-
-echo -e "\e[32m.......downloading the application code.......\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-
-echo -e "\e[32m.......changing to application directory.......\e[0m"
 rm -rf /app
 cd /app 
-
-echo -e "\e[32m.......unzip the appication code.......\e[0m"
 unzip /tmp/catalogue.zip
-
-echo -e "\e[32m.......installing npm .......\e[0m"
 npm install
-
-echo -e "\e[32m.......copying the service file.......\e[0m"
 cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
-
-echo -e "\e[32m.......start the service of catalogue.......\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue 
 systemctl restart catalogue
-
-echo -e "\e[32m.......configuring the mongo.repo.......\e[0m"
 cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
-
-echo -e "\e[32m.......installing the mongodb shell.....\e[0m"
 yum install mongodb-org-shell -y
-
-echo -e "\e[32m.......loading the scheme......\e[0m"
 mongo --host mongodb-dev.unlockers.online </app/schema/catalogue.js
