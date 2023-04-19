2c7ce9477032ea2812a7c89cac62f75687cb7a4a
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-04-19 19:05:39 +0530

    helow

--- cart.sh
+++ cart.sh
@@ -1,7 +1,5 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/comman.sh
-
 component=cart
-
 function_application
--- catalogue.sh
+++ catalogue.sh
@@ -2,7 +2,5 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/comman.sh
 component=catalogue
-
 function_application
-
 schema_fun
