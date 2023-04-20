fcb5b89756426d4e2acb154dcb092716703aec32
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-04-20 19:26:12 +0530

    file changes

--- comman.sh
+++ comman.sh
@@ -4,7 +4,25 @@ script_path=$(dirname "$script")
 
 print_msg (){
     echo -e "\e[32m.... $1 ....\e[0m"
+    echo -e "\e[32m.... $1 ....\e[0m" &>>$log_file
+
 }
+
+log_file=/tmp/roboshop.log
+
+
+stat_check_fuction () {
+    if [$1 -eq 0]
+    then 
+        echo -e "\e[32m...SUCCESS...\e[0m"
+    else
+        echo -e "\e[31m...failuer...\e[0m"
+        echo "please refer to the /tmp/roboshop.log file for more information"
+        exit 1
+    fi
+}
+
+
 
 
 
@@ -20,6 +38,7 @@ schema_fun () {
         print_msg "adding loading scheme"
         mongo --host mongodb-dev.unlockers.online </app/schema/${component}.js
     fi
+    stat_check_fuction $?
 
     if ["$schema_fun" == "mysql"]
     then
@@ -29,50 +48,61 @@ schema_fun () {
         print_msg "loading the scheme"
         mysql -h mysql-dev.unlockers.online -uroot -p${mysql_root_passwd} < /app/schema/shipping.sql 
     fi
+    stat_check_fuction $?
 }
 
 
 
 funct_prereq () {
     print_msg "add application user"  
-    useradd ${app_user}
+    useradd ${app_user} &>>$log_file
+    stat_check_fuction $?
 
     print_msg "making the directory"
-    rm -rf /app
-    mkdir /app 
+    rm -rf /app &>>$log_file
+    mkdir /app &>>$log_file 
+    stat_check_fuction $?
 
     print_msg "Downloading the content"
-    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip&>>$log_file
+    stat_check_fuction $?
 
     print_msg "changing the directory"
-    cd /app
+    cd /app &>>$log_file
+    stat_check_fuction $?
 
     print_msg "unzip the content in the app directory"
-    unzip /tmp/${component}.zip 
+    unzip /tmp/${component}.zip &>>$log_file 
+    stat_check_fuction $?
 }
 
 func_systemd_setup () {
     print_msg "adding the service file"
-    cp $script_path/${component}.service /etc/systemd/system/${component}.service
+    cp $script_path/${component}.service /etc/systemd/system/${component}.service &>>$log_file
+    stat_check_fuction $?
 
     print_msg "starting the system"
-    systemctl daemon-reload
-    systemctl enable ${component} 
-    systemctl start ${component}
-    systemctl restart shipping
+    systemctl daemon-reload &>>$log_file
+    systemctl enable ${component} &>>$log_file 
+    systemctl start ${component} &>>$log_file
+    systemctl restart shipping &>>$log_file
+    stat_check_fuction $?
 }
 
 function_nodejs () {
     print_msg "installing the repo for the node"
-    curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+    stat_check_fuction $?
 
     print_msg "installing the nodejs"
-    yum install nodejs -y
+    yum install nodejs -y &>>$log_file
+    stat_check_fuction $?
 
     funct_prereq
 
     print_msg "installing the npm install"
-    npm install
+    npm install &>>$log_file
+    stat_check_fuction $?
 
     schema_fun
 
@@ -83,29 +113,17 @@ function_nodejs () {
 
 func_java () {
     print_msg "installing the maven"
-    yum install maven -y
+    yum install maven -y >/tmp/roboshop.log &>>$log_file
 
-    if [$? -eq 0]
-    then 
-        echo -e "\e[32m...SUCCESS...\e[0m"
-    else
-        echo -e "\e[31m...failuer...\e[0m"
-        exit
-    fi
+    stat_check_fuction $?
 
     funct_prereq
 
-    print_msg "cleaning the maven"
-    mvn clean package 
-    mv target/${component}-1.0.jar ${component}.jar 
+    print_msg "cleaning the maven" &>>$log_file
+    mvn clean package &>>$log_file
+    mv target/${component}-1.0.jar ${component}.jar &>>$log_file
 
-    if [$? -eq 0]
-    then 
-        echo -e "\e[32m...SUCCESS...\e[0m"
-    else
-        echo -e "\e[31m...failuer...\e[0m"
-        exit
-    fi
+    stat_check_fuction $?
 
     schema_fun
 
