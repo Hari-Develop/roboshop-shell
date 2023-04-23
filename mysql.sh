f470ffbbc17aef04d115e1bcd9801d427096efe4
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-04-23 18:27:47 +0530

    file changes

--- mysql.sh
+++ mysql.sh
@@ -1,21 +1,38 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
+source ${script_path}/comman.sh
+mysql_root_passwd=$1
 
-echo -e "\e[32m....disable the my sql....\e[0m"
+if [ -z "$mysql_root_passwd" ] ; then
+    echo "input of mysql password is missing"
+    exit
+fi
+
+print_msg "disable the my sql"
 dnf module disable mysql -y 
+stat_check_fuction $?
 
-echo -e "\e[32m....adding the mysql repo...\e[0m"
+print_msg "adding the mysql repo"
 cp $script_path/mysql.repo /etc/yum.repos.d/mysql.repo
+stat_check_fuction $?
 
-echo -e "\e[32m....installing the community-server....\e[0m"
+
+print_msg "installing the community-server"
 yum install mysql-community-server -y
+stat_check_fuction $?
 
-echo -e "\e[32m....starting the mysql server....\e[0m"
+
+print_msg "starting the mysql server"
 systemctl enable mysqld
-systemctl start mysqld  
+systemctl start mysqld
+stat_check_fuction $?
 
-echo -e "\e[32m....changing the default passwd....\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
 
-echo -e "\e[32m....checking the passwd....\e[0m"
-mysql -uroot -pRoboShop@1
+print_msg "changing the default passwd"
+mysql_secure_installation --set-root-pass ${mysql_root_passwd}
+stat_check_fuction $?
+
+
+print_msg "checking the passwd"
+mysql -uroot -p${mysql_root_passwd}
+stat_check_fuction $?
