97f58ee3a456e51e6bcd7a4aa1ffb5e8d5fa24b8
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-04-23 18:44:21 +0530

    file changes

--- mysql.sh
+++ mysql.sh
@@ -9,30 +9,30 @@ if [ -z "$mysql_root_passwd" ] ; then
 fi
 
 print_msg "disable the my sql"
-dnf module disable mysql -y 
+dnf module disable mysql -y &>>$log_file
 stat_check_fuction $?
 
 print_msg "adding the mysql repo"
-cp $script_path/mysql.repo /etc/yum.repos.d/mysql.repo
+cp $script_path/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 stat_check_fuction $?
 
 
 print_msg "installing the community-server"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>$log_file
 stat_check_fuction $?
 
 
 print_msg "starting the mysql server"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
 stat_check_fuction $?
 
 
 print_msg "changing the default passwd"
-mysql_secure_installation --set-root-pass ${mysql_root_passwd}
+mysql_secure_installation --set-root-pass ${mysql_root_passwd} &>>$log_file 
 stat_check_fuction $?
 
 
 print_msg "checking the passwd"
-mysql -uroot -p${mysql_root_passwd}
+mysql -uroot -p${mysql_root_passwd} &>>$log_file
 stat_check_fuction $?
