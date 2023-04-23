11d1742afbc968bdda7754d9285c997fd383ca8b
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-04-23 18:59:49 +0530

    file changes

--- shipping.sh
+++ shipping.sh
@@ -3,14 +3,11 @@ script_path=$(dirname "$script")
 source ${script_path}/comman.sh
 mysql_root_passwd=$1
 
-if [-z "$mysql_root_passwd" ]
+if [ -z "$mysql_root_passwd" ]
 then
-
     echo input mysql root password is not given
     exit
-    
 fi
-
 component="shipping"
 schema_fun=mysql
 func_java
