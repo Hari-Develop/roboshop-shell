fc94e000a838100836415db3bc75b6ccdb0d1aa7
HarinadhaReddy Yadala <harinadhareddyyadala@HarinadhaReddys-MacBook-Air.local>
2023-04-23 15:12:21 +0530

    file changes

--- frontend.sh
+++ frontend.sh
@@ -4,19 +4,19 @@ source ${script_path}/comman.sh
 
 print_msg "installing the nginx server"
 yum install nginx -y &>>$log_file
-stat_check_fuction $?
+
 
 print_msg "configuring the roboshop file"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
-stat_check_fuction $?
+
 
 print_msg "remove the file existing the data"
 rm -rf /usr/share/nginx/html/* &>>$log_file
-stat_check_fuction $?
+
 
 print_msg "downlading the code file"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
-stat_check_fuction $?
+
 
 print_msg "unzip the code file"
 cd /usr/share/nginx/html &>>$log_file
@@ -26,4 +26,4 @@ print_msg "set the system set up file"
 systemctl enable nginx &>>$log_file
 systemctl start nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-stat_check_fuction $?
+
